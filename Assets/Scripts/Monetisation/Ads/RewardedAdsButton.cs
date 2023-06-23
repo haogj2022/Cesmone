fe069aaf0c39e6e7bcb0ae2206a8f023abd28bf8
haogj2022 <106388383+haogj2022@users.noreply.github.com>
2023-06-23 22:33:12 +0700

    polish the game

--- Assets/Scripts/Monetisation/Ads/RewardedAdsButton.cs
+++ Assets/Scripts/Monetisation/Ads/RewardedAdsButton.cs
@@ -6,7 +6,7 @@ public class RewardedAdsButton : MonoBehaviour, IUnityAdsLoadListener, IUnityAds
 {
     [SerializeField] Button _showAdButton;
     [SerializeField] string _androidAdUnitId = "Rewarded_Android";
-    [SerializeField] string _iOSAdUnitId = "Rewarded_iOS";
+    //[SerializeField] string _iOSAdUnitId = "Rewarded_iOS";
     string _adUnitId = null; // This will remain null for unsupported platforms
 
     [SerializeField] LevelStats levelStat;
@@ -29,14 +29,14 @@ public class RewardedAdsButton : MonoBehaviour, IUnityAdsLoadListener, IUnityAds
     public void LoadAd()
     {
         // IMPORTANT! Only load content AFTER initialization (in this example, initialization is handled in a different script).
-        Debug.Log("Loading Ad: " + _adUnitId);
+        //Debug.Log("Loading Ad: " + _adUnitId);
         Advertisement.Load(_adUnitId, this);
     }
 
     // If the ad successfully loads, add a listener to the button and enable it:
     public void OnUnityAdsAdLoaded(string adUnitId)
     {
-        Debug.Log("Ad Loaded: " + adUnitId);
+        //Debug.Log("Ad Loaded: " + adUnitId);
 
         if (adUnitId.Equals(_adUnitId))
         {
@@ -61,7 +61,7 @@ public class RewardedAdsButton : MonoBehaviour, IUnityAdsLoadListener, IUnityAds
     {
         if (adUnitId.Equals(_adUnitId) && showCompletionState.Equals(UnityAdsShowCompletionState.COMPLETED))
         {
-            Debug.Log("Unity Ads Rewarded Ad Completed");
+            //Debug.Log("Unity Ads Rewarded Ad Completed");
             // Grant a reward.
 
             levelStat.totalCoins += rewardedCoins;
--- Assets/Scripts/Monetisation/In-App Purchasing/IAPManager.cs
+++ Assets/Scripts/Monetisation/In-App Purchasing/IAPManager.cs
@@ -14,19 +14,19 @@ public class IAPManager : MonoBehaviour
         if(product.definition.id == coin5000)
         {
             //reward your players
-            Debug.Log("You've gained 5000 coins");
+            //Debug.Log("You've gained 5000 coins");
         }
 
         if (product.definition.id == coin25000)
         {
             //reward your players
-            Debug.Log("You've gained 25000 coins");
+            //Debug.Log("You've gained 25000 coins");
         }
 
         if (product.definition.id == coin100000)
         {
             //reward your players
-            Debug.Log("You've gained 100000 coins");
+            //Debug.Log("You've gained 100000 coins");
         }
     }
 
